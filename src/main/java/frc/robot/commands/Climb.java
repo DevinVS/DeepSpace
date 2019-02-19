5f8343add94756a3729622044b5e99a737bedf2a
SmithJared <smithvjared@gmail.com>
2019-02-19 16:26:21 -0700

    Organized Commands and command groups

--- src/main/java/frc/robot/commands/Climb.java
+++ src/main/java/frc/robot/commands/Climb.java
@@ -7,47 +7,30 @@
 
 package frc.robot.commands;
 
-import com.revrobotics.ControlType;
+import edu.wpi.first.wpilibj.command.CommandGroup;
 
-import edu.wpi.first.wpilibj.command.Command;
-import frc.robot.Constants;
-import frc.robot.Robot;
+public class Climb extends CommandGroup {
+  /**
+   * Add your docs here.
+   */
+  public Climb() {
+    // Add Commands here:
+    // e.g. addSequential(new Command1());
+    // addSequential(new Command2());
+    // these will run in order.
 
-public class Climb extends Command {
-  private double targetPos;
-  public Climb(double targetPos) {
-    // Use requires() here to declare subsystem dependencies
-    // eg. requires(chassis);
-    requires(Robot.lift);
-    this.targetPos = targetPos;
-  }
+    // To run multiple commands at the same time,
+    // use addParallel()
+    // e.g. addParallel(new Command1());
+    // addSequential(new Command2());
+    // Command1 and Command2 will run in parallel.
 
-  // Called just before this Command runs the first time
-  @Override
-  protected void initialize() {
-  }
+    // A command group will require all of the subsystems that each member
+    // would require.
+    // e.g. if Command1 requires chassis, and Command2 requires arm,
+    // a CommandGroup containing them would require both the chassis and the
+    // arm.
+    
 
-  // Called repeatedly when this Command is scheduled to run
-  @Override
-  protected void execute() {
-    Robot.lift.up(targetPos);
-
-  }
-
-  // Make this return true when this Command no longer needs to run execute()
-  @Override
-  protected boolean isFinished() {
-    return false;
-  }
-
-  // Called once after isFinished returns true
-  @Override
-  protected void end() {
-  }
-
-  // Called when another command which requires one or more of the same
-  // subsystems is scheduled to run
-  @Override
-  protected void interrupted() {
   }
 }
