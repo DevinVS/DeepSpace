e162d8792a890e6eb004deaaa251caa7a6748806
SmithJared <smithvjared@gmail.com>
2019-02-19 19:59:49 -0700

    Finished place command

--- src/main/java/frc/robot/commands/Place.java
+++ src/main/java/frc/robot/commands/Place.java
@@ -8,11 +8,15 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
 
 public class Place extends Command {
-  public Place() {
+  private String value;
+  public Place(String value) {
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
+    requires(Robot.io);
+    this.value = value;
   }
 
   // Called just before this Command runs the first time
@@ -23,6 +27,7 @@ public class Place extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    Robot.io.Place(value);
   }
 
   // Make this return true when this Command no longer needs to run execute()
